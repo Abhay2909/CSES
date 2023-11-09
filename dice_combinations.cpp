45a5c4827682671fee22e0bd78fc958f4fdcc7fa
Abhay Garg <54107975+Abhay2909@users.noreply.github.com>
2023-11-09 19:04:10 +0530

    Update dice_combinations.cpp
    Minor change in dice_combination.cpp code - take dp[0] as 1 instead of adding 1 in sum of dp[2] to dp[6].

--- dice_combinations.cpp
+++ dice_combinations.cpp
@@ -10,32 +10,31 @@ int32_t main(){
 	int n; cin>>n;
 	vector<int> dp;
 	dp.push_back(1);
-	for(int i = 1; i <= 5; i++) {
+	for(int i = 1; i <= 6; i++) {
 		int sum = 0;
 		for (int j = 0; j < i; j++) {
 			sum += dp[j];
 		}
-		sum += 1;
 		dp.push_back(sum);
 	}
 
 	if (n <= 6) {
-		cout << dp[n - 1];
+		cout << dp[n];
 		return 0;
 	}
 
 	for (int i = 7; i <= n; i++) {
 		int sum = 0;
-		sum += (dp[0] % mod + dp[1] % mod + dp[2] % mod + dp[3] % mod + dp[4] % mod + dp[5] % mod) % mod;
-		dp[0] = dp[1];
+		sum += (dp[1] % mod + dp[2] % mod + dp[3] % mod + dp[4] % mod + dp[5] % mod + dp[6] % mod) % mod;
 		dp[1] = dp[2];
 		dp[2] = dp[3];
 		dp[3] = dp[4];
 		dp[4] = dp[5];
-		dp[5] = sum;
+		dp[5] = dp[6];
+		dp[6] = sum;
 	}
 
-	cout << dp[5];
+	cout << dp[6];
 
 	return 0;
 
